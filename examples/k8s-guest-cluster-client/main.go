93b95e28af6b3e5b968776a0336fc2e94543d749
tribock <louis.baumann@soultec.ch>
2026-07-20 13:41:08 +0200

    fix: refactor guest cluster client initialization and add kubeconfig generation

--- examples/k8s-guest-cluster-client/main.go
+++ examples/k8s-guest-cluster-client/main.go
@@ -3,28 +3,58 @@ package main
 import (
 	"context"
 	"fmt"
+	"os"
 
 	v1 "k8s.io/api/core/v1"
+
+	"strconv"
 
 	vks_client "github.com/soultecag/vks-k8s-auth/pkg/client"
 )
 
+var (
+	port                   int
+	endpoint               string
+	username               string
+	password               string
+	targetCluster          string
+	targetClusterNamespace string
+)
+
+func init() {
+
+	//Read the VKS API server endpoint, username, and password from environment variables (SUPERVISOR_ENDPOINT, VSPHERE_USERNAME and VSPHERE_PASSWORD )
+
+	endpoint = os.Getenv("SUPERVISOR_ENDPOINT")
+	username = os.Getenv("VSPHERE_USERNAME")
+	password = os.Getenv("VSPHERE_PASSWORD")
+	portString := os.Getenv("VSPHERE_PORT")
+	targetCluster = os.Getenv("TARGET_CLUSTER")
+	targetClusterNamespace = os.Getenv("TARGET_CLUSTER_NAMESPACE")
+
+	if endpoint == "" || username == "" || password == "" {
+		panic("SUPERVISOR_ENDPOINT, VSPHERE_USERNAME and VSPHERE_PASSWORD environment variables must be set")
+	}
+
+	if portString != "" {
+		parsedPort, err := strconv.Atoi(portString)
+		if err == nil {
+			port = parsedPort
+		}
+	}
+
+}
+
 func main() {
 	// Example usage of GetK8sClientForGuestCluster
-	//
-	targetCluster := "my-guest-cluster"
-	targetClusterNamespace := "my-namespace"
-	supervisorEndpoint := "10.1.1.8"
-	username := "username@domain.local"
-	password := "myPassw0rd!"
 
 	client, err := vks_client.NewVksGuestClusterAuthClient(vks_client.VksAuthConfig{
 		GuestClusterName:      targetCluster,
 		GuestClusterNamespace: targetClusterNamespace,
-		Endpoint:              supervisorEndpoint,
+		Endpoint:              endpoint,
 		Username:              username,
 		Password:              password,
-		TlsInsecureSkipVerify: true,
+		TlsInsecureSkipVerify: false,
 	})
 	if err != nil {
 		fmt.Printf("Error creating Kubernetes client for guest cluster: %v\n", err)
@@ -40,5 +70,20 @@ func main() {
 	for _, ns := range nsList.Items {
 		fmt.Printf("- %s\n", ns.Name)
 	}
+
+	config, err := client.GenerateKubeconfig("cluster", "context")
+	if err != nil {
+		panic(err)
+	}
+
+	println("Generated kubeconfig:")
+	println(config)
+
+	// write the kubeconfig to a file
+	err = os.WriteFile("kubeconfig.yaml", []byte(config), 0644)
+	if err != nil {
+		panic(err)
+	}
+	println("Kubeconfig written to kubeconfig.yaml")
 
 }
