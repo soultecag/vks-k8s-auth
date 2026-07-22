4662232a7119aedd10370383468553ba4369e497
tribock <louis.baumann@soultec.ch>
2026-07-22 07:07:34 +0200

    fix: move files to new package

---
+++ pkg/k8s_utils/kubeconfig.go
@@ -0,0 +1,50 @@
+package k8s_utils
+
+import (
+	"fmt"
+
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
+	clientcmdapi "k8s.io/client-go/tools/clientcmd/api"
+)
+
+// ConvertRESTConfigToKubeconfig converts a *rest.Config into a valid kubeconfig YAML string
+func ConvertRESTConfigToKubeconfig(clusterName, userName, contextName string, config *rest.Config) (string, error) {
+	// Create clientcmdapi.Config object from the rest.Config
+	kubeconfigObj := clientcmdapi.Config{
+		Kind:       "Config",
+		APIVersion: "v1",
+		Clusters: map[string]*clientcmdapi.Cluster{
+			clusterName: {
+				Server:                   config.Host,
+				CertificateAuthorityData: config.TLSClientConfig.CAData,
+				InsecureSkipTLSVerify:    config.TLSClientConfig.Insecure,
+			},
+		},
+		AuthInfos: map[string]*clientcmdapi.AuthInfo{
+			userName: {
+				ClientCertificateData: config.TLSClientConfig.CertData,
+				ClientKeyData:         config.TLSClientConfig.KeyData,
+				Token:                 config.BearerToken,
+				TokenFile:             config.BearerTokenFile,
+				Username:              config.Username,
+				Password:              config.Password,
+			},
+		},
+		Contexts: map[string]*clientcmdapi.Context{
+			contextName: {
+				Cluster:  clusterName,
+				AuthInfo: userName,
+			},
+		},
+		CurrentContext: contextName,
+	}
+
+	// Marshal the object to a YAML string using clientcmd.Write
+	yamlBytes, err := clientcmd.Write(kubeconfigObj)
+	if err != nil {
+		return "", fmt.Errorf("failed to serialize kubeconfig: %w", err)
+	}
+
+	return string(yamlBytes), nil
+}
